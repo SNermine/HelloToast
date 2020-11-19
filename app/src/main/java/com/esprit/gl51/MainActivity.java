fc6ea09daa29e30da808b9998505aebbc99239a6
nermineslimane <slimane.nermine@esprit.tn>
2020-11-19 10:36:25 +0100

    Initial commit

---
+++ app/src/main/java/com/esprit/gl51/MainActivity.java
@@ -0,0 +1,44 @@
+package com.esprit.gl51;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount=0;
+    private TextView mShowCount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount=(TextView) findViewById(R.id.show_count);
+
+    }
+
+    public void countUp(View view) {
+
+        ++mCount;
+
+        if (mShowCount != null)
+
+        {
+            mShowCount.setText(Integer.toString(mCount));
+
+        }
+
+
+    }
+
+    public void showToast(View view) {
+
+        CharSequence text = "Hello Toast";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(this, text, duration);
+        toast.show();
+    }
+}
